bc7a25da74b652180f12a0749742df1b90b5441f
Jakkapan1997 <67558529+Jakkapan1997@users.noreply.github.com>
2022-04-21 19:35:33 +0700

    SET STATIC FOLDER

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-// Required express package
-const express = require('express');
+// REQUIRED PACKAGES EXPRESS
+const express = require("express");
 const app = express();
+const path = require("path");
 
-// Required dotenv package
-require('dotenv').config();
+// REQUIRED DOTENV
+require("dotenv").config();
 
-// Port
+// SETTING UP THE PORT
 const PORT = process.env.PORT || 5000;
+// SET ARRAY
+const users = [
+  {
+    id: 1,
+    name: "John",
+    email: "John@gmail.com",
+    age: 21,
+  },
+  {
+    id: 2,
+    name: "Miler",
+    email: "miler@gmail.com",
+    age: 21,
+  },
+    {
+    id: 3,
+    name: "Will",
+    email: "will@gmail.com",
+    age: 21,
+}
+];
+// SET API
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+// SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, "public")));
 
+// START THE SERVER
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    }
-);
+  console.log(`Server is running on port ${PORT}`);
+});
