4309e11fd0884decfba28a59616a9b40cd7a664a
Jakkapan1997 <67558529+Jakkapan1997@users.noreply.github.com>
2022-04-22 19:52:43 +0700

    HANDLEBARS INSTALL

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 // REQUIRED PACKAGES EXPRESS
 const express = require("express");
+
 const path = require("path");
-
-
 // REQUIRED LOGGER
 const logger = require("./middleware/logger");
-const app = express();
-// GET USERS FROM USERS.JS
+// Loads the handlebars module
+const exphbs = require("express-handlebars");
 
-// app.use('/api/users/', require('./routes/api/users'));
+// Load users from the users.js file
+const users = require("./users");
+// CREATE APP
+const app = express();
 
 // Init middleware
-// app.use(logger);
+app.use(logger);
+
+// HANDLES MIDDLEWARE
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// HOME PAGE ROUTE
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Home",
+    name: "Andrew Mead",
+    message: "Welcome to my website",
+    users
+  });
+});
+
+// BODY PARSER MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// GET USERS FROM USERS.JS
+app.use("/api/users/", require("./routes/api/users"));
+
 // REQUIRED DOTENV
 require("dotenv").config();
 
 // SETTING UP THE PORT
 const PORT = process.env.PORT || 5000;
-
 
 // SET STATIC FOLDER
 app.use(express.static(path.join(__dirname, "public")));
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,19 +1,63 @@
 // REQUIRED PACKAGES EXPRESS
 const express = require("express");
+const { UnauthorizedError } = require("express-jwt");
 const router = express.Router();
+
 const users = require("../../users");
+// REQUIRED UUID
+const uuid = require("uuid");
 // GET ALL USERS
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.json(users);
 });
 // GET SINGLE USER
-app.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const user = users.find((user) => user.id === parseInt(req.params.id));
   if (!user) {
-    // res.status(404).json({ Message: "User Not Found" });
+    res.status(404).json({ Message: "User Not Found" });
   }
   res.json(user);
 });
+// header : Content-Type: application/json
+// CREATE NEW USER
+router.post("/", (req, res) => {
+  const newUser = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+  if (!newUser.name || !newUser.email) {
+    return res.status(400).json({ Message: "Please provide a name and email" });
+  }
+  users.push(newUser);
+  res.redirect('/');
+});
+// UPDATE USER
+router.put("/:id", (req, res) => {
+  let found = users.some((user) => user.id === parseInt(req.params.id));
+  if (!found) {
+    return res.status(404).json({ Message: "User Not Found" });
+  }
+  const updateUser = req.body;
+  users.forEach((user) => {
+    if (user.id === parseInt(req.params.id)) {
+      user.name = updateUser.name ? updateUser.name : user.name;
+      user.email = updateUser.email ? updateUser.email : user.email;
 
+      res.json({ Message: "User Updated", user });
+    }
+  });
+});
+// DELETE USER
+router.delete("/:id", (req, res) => {
+  let found = users.some((user) => user.id === parseInt(req.params.id));
+  if (!found) {
+    return res.status(404).json({ Message: "User Not Found" });
+  }
+  res.json({
+    Message: "User Deleted",
+    users: users.filter((user) => user.id !== parseInt(req.params.id))
+  });
+});
 // MODULE EXPORTS
 module.exports = router;
